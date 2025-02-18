4492225f449c531bc8f213bbf6994b8316cd4c16
Om <ompately5t6@gmail.com>
2025-02-18 15:13:26 -0500

    added landing page

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
+// Toggle the mobile menu’s visibility 
+// when the hamburger ("menu-toggle") is clicked
 document.querySelector(".menu-toggle").addEventListener("click", function() {
     document.querySelector(".mobile-menu").classList.toggle("active");
   });
+  
+  // CodePen-like expansion for each wrapper:
+  document.getElementById('wrapper').onclick = function() {
+    let className = ' ' + this.className + ' ';
+    this.className = ~className.indexOf(' active ')
+      ? className.replace(' active ', ' ')
+      : this.className + ' active';
+  };
+  
+  document.getElementById('wrapper2').onclick = function() {
+    let className = ' ' + this.className + ' ';
+    this.className = ~className.indexOf(' active ')
+      ? className.replace(' active ', ' ')
+      : this.className + ' active';
+  };
+  
+  document.getElementById('wrapper3').onclick = function() {
+    let className = ' ' + this.className + ' ';
+    this.className = ~className.indexOf(' active ')
+      ? className.replace(' active ', ' ')
+      : this.className + ' active';
+  };
   
